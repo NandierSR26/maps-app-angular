a59789c5af304d6ba050153b85f5c4cb70ad1416
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-02 13:15:41 -0500

    feat: Integración de mapa con mapbox

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoibmFuZGllci1tYm94MTAiLCJhIjoiY2x6Y3hhMzViMGdldDJsb21tNTNscncxbSJ9.U0Qu_n0_I37RE-W7GW1jiA';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
