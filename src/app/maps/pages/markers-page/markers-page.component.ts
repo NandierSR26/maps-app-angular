bc4f6347c146ef346f5a71d011f50541e637b4d4
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-02 15:20:31 -0500

    feat: Funcionalidad para persistir los marcadores con LocalStorage terminada

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -7,6 +7,11 @@ interface MarkerAndColor {
   marker: Marker;
 }
 
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
+}
+
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css'],
@@ -19,7 +24,7 @@ export class MarkersPageComponent {
     -74.11197515027442,
     4.633463636471802
   );
-  public markers: MarkerAndColor[] = []
+  public markers: MarkerAndColor[] = [];
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw `El elemento HTML no fue encontrado`;
@@ -31,51 +36,70 @@ export class MarkersPageComponent {
       zoom: 10, // starting zoom
     });
 
-    // const markerHTML = document.createElement('div')
-    // markerHTML.innerHTML = 'Nandier Ruiz'
-
-    // const marker = new Marker({
-    //   element: markerHTML
-    // })
-    //   .setLngLat(this.currentLngLat)
-    //   .addTo( this.map )
-
+    this.readFromLocalStorage()
   }
 
   createMarker() {
-    if(!this.map) return
+    if (!this.map) return;
 
-    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
-    const lngLat = this.map!.getCenter()
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map!.getCenter();
 
-    this.addMarker( lngLat, color )
+    this.addMarker(lngLat, color);
   }
 
   addMarker(lngLat: LngLat, color: string) {
-    if(!this.map) return
+    if (!this.map) return;
 
     const marker = new Marker({
       color,
-      draggable: true
+      draggable: true,
     })
-      .setLngLat( lngLat )
-      .addTo( this.map )
+      .setLngLat(lngLat)
+      .addTo(this.map);
 
-      this.markers.push({
-        color,
-        marker
-      })
+    this.markers.push({
+      color,
+      marker,
+    });
+
+    this.saveToLocalStorage()
   }
 
-  deleteMarker( index: number ) {
+  deleteMarker(index: number) {
     this.markers[index].marker.remove();
-    this.markers.splice(index, 1)
+    this.markers.splice(index, 1);
   }
 
-  flyTo( marker: Marker ) {
+  flyTo(marker: Marker) {
     this.map?.flyTo({
       zoom: 14,
-      center: marker.getLngLat()
+      center: marker.getLngLat(),
+    });
+  }
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map(({ color, marker }) => {
+      return {
+        color,
+        lngLat: marker.getLngLat().toArray()
+      }
+    })
+
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers))
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]'
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString)
+    console.log({plainMarkers})
+
+    plainMarkers.forEach( ({ color, lngLat }) => {
+      const [ lng, lat ] = lngLat
+      const coords = new LngLat(lng, lat)
+      this.addMarker( coords, color )
     })
   }
 }
