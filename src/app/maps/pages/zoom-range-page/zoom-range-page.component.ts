bec1b4fcfd89c07f587e8c629be3a4f748ec7a2f
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-02 14:04:47 -0500

    feat: Funcionalidad para obtener las coordenadas centrales del mapa

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,15 +1,16 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap?: ElementRef
 
   public zoom: number = 10
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.11197515027442, 4.633463636471802)
 
   ngAfterViewInit(): void {
 
@@ -18,11 +19,15 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners()
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove()
   }
 
   mapListeners() {
@@ -35,6 +40,10 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map.on('zoomend', (e) => {
       if(this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18)
+    })
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter()
     })
   }
 
